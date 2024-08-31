f54f6b51f505af12c6265cae67ac45d357f53c42
mumzaislam <izzahmena@gmail.com>
2024-09-01 01:28:40 +0500

    complete

---
+++ cal.py
@@ -0,0 +1,10 @@
+a:int =int(input('enter your number:'))
+b:int= int(input("enter your number:"))
+c = a+b
+print(c)
+d = a-b
+print(d)
+e = a/b
+print(e)
+f = a*b
+print(f)
---
+++ var.py
@@ -0,0 +1,122 @@
+# Q1 Simple message
+x = "Hello world!!"
+print(x)
+
+# Q2 Simple message
+
+x = "silence is the best answer"
+print(x)
+x = "hard work is the key to success"
+print(x)
+
+# Q3 Personal message
+
+x:str = "Eric"
+print(f'"hello {x}, would you like to learn some Python today?"') 
+
+# Q4 Name case
+
+person = "faizan naeem"
+print(person.upper())
+person = "faizan naeem"
+print(person.lower())
+person = "faizan naeem"
+print(person.title())
+
+# Q5 Famous quote
+
+person = "Quaid"
+quote = "work hard is the key to success"
+person_qoute = f'("{person} once said , "{quote}")'
+print(person_qoute)
+ 
+# Q6 Famous quote 2
+
+famous_person ="Quaid"
+message = "work hard is key to success"
+print(message)
+
+# Q7 Striping names]
+
+name:str = "    Quaid Azam     "
+print("Quaid\tAzam")
+print("Quaid\nAzam")
+print(name.rstrip())
+print(name.lstrip())
+print(name.strip())
+
+
+# Q8 variable sum
+
+x:int = 5
+y:int = 10
+z:int = 15
+sum = (x+y+z)
+print(sum)
+
+# Q9 Swap variable
+
+x,y = 1,2
+print("before swapping")
+print(x,y)
+x,y = y,x
+print("after swapping")
+print(x,y)
+
+# Q10 Favorite colour
+
+colour:str = "purple"
+fvrt_clr:str = "purple"
+print(fvrt_clr)
+
+
+# Q11 Changing pet Name
+
+pet_name:str = "Buddy"
+pet_name:str = "MAX"
+print(pet_name)
+
+# Q13 Reassigning score
+
+score:int = 100
+print(score)
+score:int = 200
+print(score)
+
+# Q14 City Name
+
+city:str ="lahore"
+print(city)
+
+# Q15
+text:str = "python programming"
+print(text.title())
+
+# Q16
+text:str = "python programming"
+print(text.lower())
+
+# Q17
+
+text:str = "python programming"
+print(text.upper())
+
+# Q18 Current temperature
+
+temperature:int = 25
+print(f"The current temperature is {temperature} degrees .")
+      
+# Q19 Printing a poem
+
+poem:str = """Rain rain go away,
+come again another day ,
+little johnny wants to play,
+rain rain go away."""
+print(poem)
+
+Q12 OBSERVING ERROR
+
+str = "sunshine"
+print(x)
+sunshine "sunsine"
+print(x)x:
